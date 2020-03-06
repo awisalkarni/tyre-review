c4ae9f96dabf7deb1350aaca300e7cc5ff1664ae
awis-mbp <awis@awislabs.com>
2020-03-06 09:21:02 +0800

    npm

--- config/deploy.php
+++ config/deploy.php
@@ -51,7 +51,8 @@ return [
         
         // Code and composer vendors are ready but nothing is built.
         'build' => [
-            //
+            'npm:install',
+            'npm:production',
         ],
         
         // Deployment is done but not live yet (before symlink)
